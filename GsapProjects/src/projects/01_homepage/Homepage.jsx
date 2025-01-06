72a1a185c07ce732f73e5326731298257d3056f5
Rahul Kumar <kumarrahul771996@gmail.com>
2025-01-07 01:44:37 +0530

    add: gsap/react installation + homepage animation

--- GsapProjects/src/projects/01_homepage/Homepage.jsx
+++ GsapProjects/src/projects/01_homepage/Homepage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(useGSAP);
 
 const Homepage = () => {
+  const h1Ref = useRef(null);
+
+  useGSAP(() => {
+    gsap.fromTo(
+      h1Ref.current,
+      { opacity: 0, y: -50 },
+      { opacity: 1, y: 0, duration: 1.5, ease: 'power3.out' }
+    );
+  }, [h1Ref]);
+
   return (
     <div>
-      <h1>Hello World from GSAP project with React + Vite</h1>
+      <h1 ref={h1Ref}>Hello World from GSAP project with React + Vite</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
